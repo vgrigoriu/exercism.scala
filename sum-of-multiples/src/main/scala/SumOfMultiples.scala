0114e87ad0cc6aa346bfcd4a95a7ed5d7fb4e592
Victor Grigoriu <grigoriu@adobe.com>
2019-03-05 17:56:21 +0200

    Sum of Multiples: enable tests

--- sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,11 +1,9 @@
 object SumOfMultiples {
   def sum(factors: Set[Int], limit: Int): Int = {
-  	def isMultiple(x: Int) = factors.exists(x % _ == 0)
+    def isMultiple(x: Int) = factors.exists(x % _ == 0)
 
-  	val multiples = for (x <- 1 until limit; if isMultiple(x)) yield x
+    val multiples = for (x <- 1 until limit; if isMultiple(x)) yield x
 
-  	multiples.sum
+    multiples.sum
   }
-
 }
-
