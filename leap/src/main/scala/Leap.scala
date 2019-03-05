6ccee8f5e439838439c739bfc089551d1543f077
Victor Grigoriu <grigoriu@adobe.com>
2019-03-05 17:49:40 +0200

    git init

---
+++ leap/src/main/scala/Leap.scala
@@ -0,0 +1,7 @@
+// on every year that is evenly divisible by 4
+//  except every year that is evenly divisible by 100
+//    unless the year is also evenly divisible by 400
+
+object Leap {
+  def leapYear(year: Int): Boolean = year % 4 == 0 && year % 100 != 0 || year % 400 == 0
+}
---
+++ sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -0,0 +1,11 @@
+object SumOfMultiples {
+  def sum(factors: Set[Int], limit: Int): Int = {
+  	def isMultiple(x: Int) = factors.exists(x % _ == 0)
+
+  	val multiples = for (x <- 1 until limit; if isMultiple(x)) yield x
+
+  	multiples.sum
+  }
+
+}
+
