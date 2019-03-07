a9f34cbd59b7bdd7db7a97efb5657244cf4fc480
Victor Grigoriu <grigoriu@adobe.com>
2019-03-07 13:20:46 +0200

    Bob

--- bob/src/main/scala/Bob.scala
+++ bob/src/main/scala/Bob.scala
@@ -1,3 +1,25 @@
 object Bob {
-  def response(statement: String): String = ???
+  def response(statement: String): String =
+    if (isSilence(statement)) {
+      "Fine. Be that way!"
+    } else if (isShouting(statement) && isQuestion(statement)) {
+      "Calm down, I know what I'm doing!"
+    } else if (isShouting(statement)) {
+      "Whoa, chill out!"
+    } else if (isQuestion(statement)) {
+      "Sure."
+    } else {
+      "Whatever."
+    }
+
+  def isSilence(statement: String): Boolean =
+    statement.trim.isEmpty
+
+  def isShouting(statement: String): Boolean = {
+    val letters = statement.filter(_.isLetter)
+    !letters.isEmpty && letters.forall(_.isUpper)
+  }
+
+  def isQuestion(statement: String): Boolean =
+    statement.trim.last == '?'
 }
