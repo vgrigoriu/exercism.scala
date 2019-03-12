0e11c84ff353735a44eb2db928c706459c2d271d
Victor Grigoriu <grigoriu@adobe.com>
2019-03-12 09:36:03 +0200

    Tail-recursive version

--- accumulate/src/main/scala/Accumulate.scala
+++ accumulate/src/main/scala/Accumulate.scala
@@ -1,6 +1,10 @@
 class Accumulate {
   def accumulate[A, B](f: A => B, list: List[A]): List[B] =
-    list.foldRight(List[B]()) { (item, result) =>
-      f(item) :: result
+    map(f, list, List())
+
+  def map[A, B](f: A => B, input: List[A], acc: List[B]): List[B] =
+    input match {
+      case Nil          => acc
+      case head :: tail => map(f, tail, acc :+ f(head))
     }
 }
