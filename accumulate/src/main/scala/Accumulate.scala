5acd8c3dec23d25da10ea32b9b0a367171e32040
Victor Grigoriu <grigoriu@adobe.com>
2019-03-12 18:12:19 +0200

    Use ListBuffer

--- accumulate/src/main/scala/Accumulate.scala
+++ accumulate/src/main/scala/Accumulate.scala
@@ -1,8 +1,10 @@
+import scala.collection.mutable.ListBuffer
+
 class Accumulate {
   def accumulate[A, B](f: A => B, list: List[A]): List[B] =
-    map(f, list, List())
+    map(f, list, ListBuffer()).toList
 
-  def map[A, B](f: A => B, input: List[A], acc: List[B]): List[B] =
+  def map[A, B](f: A => B, input: List[A], acc: ListBuffer[B]): ListBuffer[B] =
     input match {
       case Nil          => acc
       case head :: tail => map(f, tail, acc :+ f(head))
