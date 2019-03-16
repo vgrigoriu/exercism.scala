d2770b41c0b3762cb93fe32e8e0715d41022ca98
Victor Grigoriu <grigoriu@adobe.com>
2019-03-16 08:00:27 +0200

    for FTW

--- etl/src/main/scala/Etl.scala
+++ etl/src/main/scala/Etl.scala
@@ -3,13 +3,9 @@ object Etl {
     invert(scoresToLetter, (l: String) => l.toLowerCase)
 
   private def invert[A, B, C](oneToMany: Map[A, Seq[B]],
-                              transformation: B => C): Map[C, A] =
-    oneToMany.foldLeft(Map[C, A]()) { (cToA, keyToList) =>
-      keyToList match {
-        case (key, list) =>
-          list.foldLeft(cToA) { (cToA, item) =>
-            cToA + (transformation(item) -> key)
-          }
-      }
-    }
+                               transformation: B => C): Map[C, A] =
+    for {
+      (key, list) <- oneToMany
+      item <- list
+    } yield transformation(item) -> key
 }
