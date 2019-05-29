2fc98d2528ea29b804862eb6cc01eee4b3924426
Victor Grigoriu <grigoriu@adobe.com>
2019-05-29 17:26:41 +0300

    flatten-array somewhat type-safe solution

--- flatten-array/src/main/scala/FlattenArray.scala
+++ flatten-array/src/main/scala/FlattenArray.scala
@@ -1,8 +1,27 @@
+sealed trait ListOrLists[+A]
+
+case object LoLNil extends ListOrLists[Nothing] {}
+case class LoLCons[A](head: A, tail: ListOrLists[A]) extends ListOrLists[A]
+case class LoLConsList[A](head: ListOrLists[A], tail: ListOrLists[A]) extends ListOrLists[A]
+
 object FlattenArray {
-  def flatten(list: List[Any]): List[Any] =
-    list.flatMap(thing => thing match {
-      case l: List[Any] => flatten(l)
-      case null => List()
-      case o: Any => List(o)
-    })
+  def flatten(list: List[_]): List[Int] = innerFlatten(unsafeToLoLInt(list))
+
+  private def innerFlatten[A](list: ListOrLists[A]): List[A] = {
+    println(list)
+    val result = list match {
+      case LoLNil => List()
+      case LoLCons(head, tail) => head :: innerFlatten(tail)
+      case LoLConsList(head, tail) => innerFlatten(head) ++ innerFlatten(tail)
+    }
+    println(result)
+    result
+  }
+
+  private def unsafeToLoLInt[A](list: List[_]): ListOrLists[Int] = list match {
+    case Nil => LoLNil
+    case (head: List[_]) :: tail => LoLConsList(unsafeToLoLInt(head), unsafeToLoLInt(tail))
+    case (head: Int) :: tail => LoLCons(head, unsafeToLoLInt(tail))
+    case null :: tail => unsafeToLoLInt(tail)
+  }
 }
